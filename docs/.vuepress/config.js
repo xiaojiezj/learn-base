a871e5b70f217c36c52f8b8a6bd57f0910e6d020
DanielDemi <799732469@qq.com>
2020-02-23 23:00:54 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    title: '学习笔记',
+    description: '每天学一点，进步一小步',
+    themeConfig: {
+        navbar: true,
+        sidebar: [
+            {
+                title: 'html',   // 必要的
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                ]
+            },
+            {
+                title: 'css',   // 必要的
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                ]
+            },
+            {
+                title: 'js',   // 必要的
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                ]
+            },
+            {
+                title: 'ts',   // 必要的
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                ]
+            }
+        ]
+    }
+}
